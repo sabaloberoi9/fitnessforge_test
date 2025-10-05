7c2e130ef7333de55e7273701865c4db06e4a4cc
sabaloberoi9 <145396476+sabaloberoi9@users.noreply.github.com>
2025-10-05 14:32:18 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This tells Vite where to find our secure function
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+      },
+    },
+  },
+})
